122f7d53689a7f98ec9b2e8fd410f4ca62847b5b
Travis Mick <root@lo.calho.st>
2015-08-20 13:47:39 -0600

    install jadhoc on nodes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import session_helper, graph_helper
 import argparse, itertools, time
+from core import service
+from jadhoc import Jadhoc
 
 def test_main(filename):
         """ Imports a topology from the given XML file and tests communication
@@ -11,23 +13,29 @@ def test_main(filename):
         
         # get nodes
         nodes = session.get_nodes()
+        
+        # install jadhoc service on each node
+        svcs = session.services #service.CoreServices(session)
+        for n in nodes:
+                svcs.addservicestonode(n, None, "Jadhoc|IPForward", False)
+                session.services.bootnodeservices(n)
+        
+        for n in nodes:
+                print("%s" % n.name)
+                n.icmd(["ps", "aux"])
+                print("")
 
-        # test communication between each pair of nodes
-        #for x, y in itertools.product(nodes, nodes):
-        #        if x != y:
-        #                addr_x = session_helper.addr_of(x)
-        #                addr_y = session_helper.addr_of(y)
-        #                print("ping %s -> %s" % (addr_x, addr_y))
-        #                x.icmd(["bash", "-c", "ping -c 1 %s | tail -2 | head -1" % addr_y])
-        #                print("")
-        #session.get_adjacencies(nodes[0])
+        # test communication between each pair of nodes in each connected component
         g = graph_helper(nodes, session.get_adjacencies)
         cc = g.dfs_connected_components()
         for c in cc:
-                print("New component: ")
-                for n in c:
-                        print(n.name)
-                print("")
+                for x, y in itertools.product(c, c):
+                        if x != y:
+                                addr_x = session_helper.addr_of(x)
+                                addr_y = session_helper.addr_of(y)
+                                print("ping %s -> %s" % (addr_x, addr_y))
+                                x.icmd(["bash", "-c", "ping -c 10 %s | tail -2 | head -1" % addr_y])
+                                print("")
 
         # end scenario
         session.shutdown()
